63afa3b74e3b97bbf0210d0c3b832445fa40e2ef
elharraneamine <mohamedamineelharrane@hotmail.com>
2023-09-13 15:18:24 -0700

    t

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-  int i, letter;
+int i, letter;
 
-  for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
     {
-      for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
-      _putchar('\n');
+_putchar('\n');
     }
 }
